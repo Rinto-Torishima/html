909610893ab790d384c6709dd8686a6c84254985
Rinto-Torishima <paru.22.pen@icloud.com>
2023-02-10 17:50:33 +0900

    変更

--- script.js
+++ script.js
@@ -4,15 +4,3 @@ var app = new Vue({
     ActiveBtn: false,
   },
 });
-$(function () {
-  $(".fit").hover(
-    function () {
-      //マウスオーバーしたときの処理
-      $(this).stop().animate({ opacity: 0.5 }, "fast");
-    },
-    function () {
-      //マウスアウトしたときの処理
-      $(this).stop().animate({ opacity: 1 }, "fast");
-    }
-  );
-});
