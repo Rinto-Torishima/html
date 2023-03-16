b1aed0506cb2dc86caed5186f69fcc89f642170d
Rinto-Torishima <paru.22.pen@icloud.com>
2023-03-16 16:03:31 +0900

    変更

--- script/script.js
+++ script/script.js
@@ -3,11 +3,17 @@ function toggleNav() {
   const body = document.body;
   const hamburger = document.getElementById("js_hamburger");
   const overlay = document.getElementById("js_overlay");
+  const a = document.querySelectorAll(".atag");
   hamburger.addEventListener("click", function () {
     body.classList.toggle("nav_open"); //クラスが含まれていれば削除、含まれていなければ追加する
   });
   overlay.addEventListener("click", function () {
     body.classList.remove("nav_open"); //クラスを削除する
+  });
+  a.forEach(function (element) {
+    element.addEventListener("click", function () {
+      body.classList.remove("nav_open");
+    });
   });
 }
 toggleNav();
